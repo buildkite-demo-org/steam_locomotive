deb22cbe612791af0567538041ab8d8dd5554bfe
diogofraga <kdfraga@gmail.com>
2024-11-25 15:11:19 +0000

    finally

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -10,11 +10,3 @@ find specs/* -type d | while read -r D; do
   echo "  - command: \"$D/test.sh\""
   echo "    label: \"$(basename "$D")\""
 done
-
-# A deploy step only if it's the master branch
-
-if [[ "$BUILDKITE_BRANCH" == "master" ]]; then
-  echo "  - wait"
-  echo "  - command: \"echo Deploy!\""
-  echo "    label: \":rocket:\""
-fi
